c230656113bb13c60b133871c9330eb6341db954
sarosek-yury <sarosek_ja_18@mf.grsu.by>
2021-04-07 12:45:38 +0300

    add git ignore

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const PORT = process.env.PORT || 8081;
 
 const express = require("express");
-
+const fs = require('fs');
 const app = express();
+
+app.get('/api/history/all', async function(req, res) {
+    req.send('OK');
+});
 
 app.use(express.static(__dirname + "/static"));
 app.listen(PORT);
